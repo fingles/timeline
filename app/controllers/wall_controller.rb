e9ad521dc65bc34bd4ee26b94edfba7481d36337
lyy <yongred@nate.com>
2017-02-14 06:04:57 +0000

    save posts, change page

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -3,10 +3,17 @@ class WallController < ApplicationController
   end
 
   def write_complete
+	p = Post.new
+	p.name = params[:writer]
+	p.contetn = params[:content]
+	if p.save
+		redirect_to "/wall/posts"
+	else
+		flash[:alert] = p.errors[:content][0]
+		redirect_to :back
+	end
   end
 
   def posts
-	@writer = params[:writer]
-	@content = params[:content]
   end
 end
